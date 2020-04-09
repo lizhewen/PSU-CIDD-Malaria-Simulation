fa5888fe4713da3ec6465726e4864128ff61a820
Eric Zhewen Li <32529051+lizhewen@users.noreply.github.com>
2020-04-09 10:08:12 -0400

    update cluster vcpkg path

--- cluster_build.sh
+++ cluster_build.sh
@@ -2,5 +2,5 @@
 git pull
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/storage/home/uut47/work/dev_env/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/storage/home/zxl163/work/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 make -j 8
